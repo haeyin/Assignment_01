50de35cfb4aea108964ac8a53fc6993fab737e88
Clare Shin <haeyin.shin@gmail.com>
2020-02-06 23:41:29 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems" + "\n\r" + "Remember, we're \"accounting\" on you!");
+        Console.ReadLine();
+        }
+    }
